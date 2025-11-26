b66e76f0d359241bf69bdc691897fab679c682bc
Cristian Figueroa <figueroaz.ca@gmail.com>
2025-11-25 21:01:13 -0500

    Instanciar servidor, crear .gitignore

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server runnig on http://localhost:${PORT}`);
+});
+
 console.log("Project Moto BTA");

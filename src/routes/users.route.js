2291f86babc3f384e8981bb87a258fd039e3b573
Cristian Figueroa <figueroaz.ca@gmail.com>
2025-11-26 21:44:04 -0500

    Actualizar rutas

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const router = express.Router();
 
 router.get(`/`, (req, res) => {
-  res.send(`<h1>Users</h1>`);
+  res.json([{ name: `Gabo`, edad: 14 }]);
 });
 
 module.exports = router;

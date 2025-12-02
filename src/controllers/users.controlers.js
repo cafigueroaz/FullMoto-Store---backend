d9e90ce8340650d91291000a7a53b7f193fc9267
Cristian Figueroa <figueroaz.ca@gmail.com>
2025-12-02 18:20:53 -0500

    Refactor: Mejorar manejo de errores en la creación de usuarios

--- src/controllers/users.controlers.js
+++ src/controllers/users.controlers.js
@@ -2,17 +2,22 @@ import Usermodel from "../models/User.model.js";
 import { registerUser } from "../services/user.services.js";
 
 const createUser = async (req, res) => {
-const data = req.body;
+  try {
+    const data = req.body;
 
-  console.log(data);
+    console.log(data);
 
-  const dataRegistered = await registerUser(data);
+    const dataRegistered = await registerUser(data);
 
-  res.json({
-    msg: "Se creo un usuario",
-    dataRegistered
-  });
-
-}
-
+    res.json({
+      msg: "Se creo un usuario",
+      dataRegistered,
+    });
+  } catch {
+    console.error(error);
+    res.json({
+      msg: "Error: no se pudo crear el usuario",
+    });
+  }
+};
 export { createUser };

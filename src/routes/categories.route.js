3c07ae3b9906c785b581c11f9942b9475518412b
GabrielH <gabohernandezplazas27@gmail.com>
2025-12-15 20:49:40 -0500

    Agregar funcionalidad de gestión de categorías: implementar controladores, rutas y servicios para crear, obtener y eliminar categorías

---
+++ src/routes/categories.route.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { createCategory, getAllCategories, getCategoryById, deleteCategoryById} from "../controllers/categories.controllers.js"
+import authenticationUser from "../middlewares/authentication.middlewares.js";
+import authorizationUser from "../middlewares/authorization.middlewares.js";
+
+const router = Router();
+
+router.post("/created", createCategory);
+router.get("/get-all", getAllCategories);
+router.get("/get-by-id/:idcategory", getCategoryById);
+router.delete("/delete/:idcategory", [authenticationUser, authorizationUser], deleteCategoryById);
+
+export default router;
---
+++ src/services/categories.services.js
@@ -0,0 +1,19 @@
+import categoriesModel from "../models/categories.model.js";
+
+const dbCreateCategory = async (newCategory) => {
+  return await categoriesModel.create(newCategory);
+}
+
+const dbGetAllCategories = async () => {
+  return await categoriesModel.find();
+}
+
+const dbGetCategoryById = async (_id) => {
+  return await categoriesModel.findOne({ _id });
+}
+
+const dbDeleteCategoryById = async (_id) => {
+  return await categoriesModel.findOneAndDelete({ _id });
+}
+
+export { dbCreateCategory, dbGetAllCategories, dbGetCategoryById, dbDeleteCategoryById };

7a2adcde2f6a35fe7a225d5f9b6e04a44d347344
Cristian Figueroa <figueroaz.ca@gmail.com>
2026-03-16 14:53:51 -0500

    feat: Corregir autentificacion de las rutas de categorias.

--- src/services/categories.services.js
+++ src/services/categories.services.js
@@ -2,22 +2,27 @@ import categoriesModel from "../models/categories.model.js";
 
 const dbCreateCategory = async (newCategory) => {
   return await categoriesModel.create(newCategory);
-}
+};
 
 const dbGetAllCategories = async () => {
   return await categoriesModel.find();
-}
+};
 
 const dbGetCategoryById = async (_id) => {
   return await categoriesModel.findOne({ _id });
-}
+};
 
 const dbDeleteCategoryById = async (_id) => {
   return await categoriesModel.findOneAndDelete({ _id });
-}
+};
 
-const dbUpdateCategoryById = async (_id) => {
-  return await categoriesModel.findByIdAndUpdate(_id, { new: true });
-}
-
-export { dbCreateCategory, dbGetAllCategories, dbGetCategoryById, dbDeleteCategoryById, dbUpdateCategoryById};
+const dbUpdateCategoryById = async (_id, data) => {
+  return await categoriesModel.findByIdAndUpdate(_id, data, { new: true });
+};
+export {
+  dbCreateCategory,
+  dbGetAllCategories,
+  dbGetCategoryById,
+  dbDeleteCategoryById,
+  dbUpdateCategoryById,
+};
